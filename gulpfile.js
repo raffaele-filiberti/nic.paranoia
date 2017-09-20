9f12405e75c30d42186260a23399f690bd3b56e5
Raffaele Filiberti <raffaele.filiberti@frozenbyte.it>
2017-09-20 19:40:32 +0200

    inital release

---
+++ gulpfile.js
@@ -0,0 +1,55 @@
+// Include gulp
+var gulp = require('gulp');
+// Include browserSync
+var browserSync = require('browser-sync').create();
+
+// Include Our Plugins
+var jshint = require('gulp-jshint');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+
+
+// Lint Task
+gulp.task('lint', function() {
+    return gulp.src('js/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
+
+// Compile Our Sass
+gulp.task('sass', function() {
+    return gulp.src('scss/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('dist/css'))
+        .pipe(browserSync.stream());
+});
+
+// Concatenate & Minify JS
+gulp.task('scripts', function() {
+    return gulp.src('js/*.js')
+        .pipe(concat('all.js'))
+        .pipe(gulp.dest('dist'))
+        .pipe(rename('all.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js'))
+        .pipe(browserSync.stream());
+});
+
+// Watch Files For Changes
+gulp.task('watch', function() {
+    gulp.watch('js/*.js', ['lint', 'scripts']);
+    gulp.watch('scss/*.scss', ['sass']);
+    gulp.watch("*.html").on('change', browserSync.reload);
+});
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['lint', 'sass', 'scripts', 'watch'], function() {
+    browserSync.init({
+        server: "./"
+    });
+});
+
+//default task
+gulp.task('default', ['serve']);
